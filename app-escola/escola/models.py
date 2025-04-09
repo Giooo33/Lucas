4b334de2fd81d615e6498e79b73c7329ca2372a2
Giovana <giovanadutramaga@gmail.com>
2025-04-09 08:14:52 -0300

    Adiciona requirements.txt

--- app-escola/escola/models.py
+++ app-escola/escola/models.py
@@ -24,6 +24,23 @@ class Turma(models.Model):
         ("JG", "Jogos"),
     )
 
+class Materia(models.Model):
+    MATERIA_CHOICE = (
+        ("Desenvolvimento de Sistemas"),
+        ("Ciências da Natureza"),
+        ("Jogos"),
+        ("Matemática"),
+        ("Português"),
+        ("Educação Física"),
+        ("História"),
+        ("Geografia"),
+        ("Artes"),
+        ("Filosofia"),
+        ("Sociologia"),
+        ("Inglês"),
+        ("Espanhol"),
+    )
+
     turma_nome = models.CharField(max_length=50, choices=TURMA_CHOICE, verbose_name="Selecione a turma")
     itinerario_nome = models.CharField(max_length=50, choices=ITINERARIO_CHOICE, verbose_name="Selecione o itinerário")
     
@@ -31,8 +48,7 @@ class Turma(models.Model):
 
 
 class Responsavel(models.Model):
-    nome_responsavel = models.CharField(max_length=50, verbose_name="Digite o nome do responsável")
-    sobrenome_responsavel= models.CharField(max_length=50, verbose_name="Digite o sobrenome do responsável")
+    nome_completo_responsavel = models.CharField(max_length=200, verbose_name="Digite o nome do responsável completo")
     numero_telefone_responsavel = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_responsavel = models.EmailField(max_length=100, verbose_name="Digite o e-mail do responsável")
     adress = models.CharField(max_length=100) 
@@ -44,8 +60,7 @@ class Responsavel(models.Model):
     
 class Aluno(models.Model):
 
-    nome_aluno = models.CharField(max_length=50, verbose_name="Digite o nome do aluno")
-    sobrenome_aluno= models.CharField(max_length=50, verbose_name="Digite o sobrenome do aluno")
+    nome_completo_aluno = models.CharField(max_length=50, verbose_name="Digite o nome do aluno completo")
     numero_telefone_aluno = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_aluno = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
     adress = models.CharField(max_length=100) 
@@ -60,8 +75,7 @@ class Aluno(models.Model):
 
 class Professor(models.Model):
 
-    nome_professor = models.CharField(max_length=50, verbose_name="Digite seu nome")
-    sobrenome_professor= models.CharField(max_length=50, verbose_name="Digite o seu sobrenome")
+    nome_completo_professor = models.CharField(max_length=50, verbose_name="Digite seu nome completo")
     numero_telefone_professor = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_professor = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
     adress = models.CharField(max_length=100) 
@@ -69,6 +83,8 @@ class Professor(models.Model):
     data_nascimento_professor = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do professor")
     numero_matricula_professor = models.CharField(max_length=6, verbose_name="Digite o número da matricula")
     class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_professor", blank=True, null=False)    
+    class_choices = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="materia", blank=True, null=False) 
+
 
 #O ForeignKey é uma chave estrangeira que estabelece um relacionamento entre duas tabelas, permitindo que você vincule registros de uma tabela a registros de outra tabela. 
 # O parâmetro on_delete especifica o que acontece quando o registro relacionado é excluído. 
