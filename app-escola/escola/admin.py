2bc73945742530019e09622500a66b2aefedfa13
Giovana <giovanadutramaga@gmail.com>
2025-04-09 12:04:33 -0300

    0904

--- app-escola/escola/admin.py
+++ app-escola/escola/admin.py
@@ -1,33 +1,42 @@
 from django.contrib import admin
-from escola.models import Responsavel, Aluno, Professor
+from escola.models import Responsavel, Aluno, Professor, Turma, Materia
 
 class ResponsaveisAdmin(admin.ModelAdmin):
-    list_display =('nome_responsavel', 'sobrenome_responsavel', 'numero_telefone_responsavel', 'email_responsavel', 'CPF_responsavel', 'data_nascimento_responsavel' )
-    list_display_links = ('nome_responsavel', 'sobrenome_responsavel', 'numero_telefone_responsavel', 'email_responsavel')
-    search_fields = ('nome_responsavel', 'sobrenome_responsavel')
-    list_filter = ('nome_responsavel', 'sobrenome_responsavel')
+    list_display =('nome_completo_responsavel', 'numero_telefone_responsavel', 'email_responsavel', 'CPF_responsavel', 'data_nascimento_responsavel' )
+    list_display_links = ('nome_completo_responsavel', 'numero_telefone_responsavel', 'email_responsavel')
+    search_fields = ('nome_completo_responsavel',)
+    list_filter = ('nome_completo_responsavel',)
+
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'turma_nome', 'itinerario_nome')
+    search_fields = ('turma_nome', 'itinerario_nome',)
+    list_filter = ('turma_nome', 'itinerario_nome',)
     
-    
+class MateriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'materia_nome')
+    search_fields = ('materia_nome',)
+    list_filter = ('materia_nome',)
+   
 class AlunoAdmin(admin.ModelAdmin):
-    list_display =('nome_aluno', 'sobrenome_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno', 'CPF_aluno', 'data_nascimento_aluno', 'class_choices' )
-    list_display_links = ('nome_aluno', 'sobrenome_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno')
-    search_fields = ('nome_aluno', 'sobrenome_aluno')
-    list_filter = ('nome_aluno', 'sobrenome_aluno')
-    
-    
-    
+    list_display =('nome_completo_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno', 'CPF_aluno', 'data_nascimento_aluno', 'class_choices' )
+    list_display_links = ('nome_completo_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno')
+    search_fields = ('nome_completo_aluno',)
+    list_filter = ('nome_completo_aluno',)
     
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display =('nome_professor', 'sobrenome_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor', 'CPF_professor', 'data_nascimento_professor', 'class_choices' )
-    list_display_links = ('nome_professor', 'sobrenome_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor')
-    search_fields = ('nome_professor', 'sobrenome_professor')
-    list_filter = ('nome_professor', 'sobrenome_professor')
+    list_display =('nome_completo_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor', 'CPF_professor', 'data_nascimento_professor', 'subject_choices' )
+    list_display_links = ('nome_completo_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor')
+    search_fields = ('nome_completo_professor',)
+    list_filter = ('nome_completo_professor',)
     
     
     
     
     
     #Registrar as classes no Django Admin
+
+admin.site.register(Turma, TurmaAdmin)
 admin.site.register(Responsavel,  ResponsaveisAdmin)
 admin.site.register(Aluno,  AlunoAdmin)
 admin.site.register(Professor,  ProfessorAdmin)
+admin.site.register(Materia, MateriaAdmin)
--- app-escola/escola/models.py
+++ app-escola/escola/models.py
@@ -1,20 +1,11 @@
 from django.db import models
-from validate_docbr import CPF
-from django.core.exceptions import ValidationError
-
-def cpf_validate(value):
-    cpf = CPF()
-    if not cpf.validate(value):
-        raise ValidationError(
-            ("%(value)s is not a valid CPF"),
-            params={"value": value},
-    )  
+from escola.validate import cpf_validate, telefone_validate
 
 class Turma(models.Model):
     TURMA_CHOICE = (
-        ("1", "1° Ano"),
-        ("2", "2° Ano"),
-        ("3", "3° Ano"),
+        ("1°", "1° Ano"),
+        ("2°", "2° Ano"),
+        ("3°", "3° Ano"),
 
     )  
 
@@ -24,66 +15,71 @@ class Turma(models.Model):
         ("JG", "Jogos"),
     )
 
-class Materia(models.Model):
-    MATERIA_CHOICE = (
-        ("Desenvolvimento de Sistemas"),
-        ("Ciências da Natureza"),
-        ("Jogos"),
-        ("Matemática"),
-        ("Português"),
-        ("Educação Física"),
-        ("História"),
-        ("Geografia"),
-        ("Artes"),
-        ("Filosofia"),
-        ("Sociologia"),
-        ("Inglês"),
-        ("Espanhol"),
-    )
-
     turma_nome = models.CharField(max_length=50, choices=TURMA_CHOICE, verbose_name="Selecione a turma")
     itinerario_nome = models.CharField(max_length=50, choices=ITINERARIO_CHOICE, verbose_name="Selecione o itinerário")
-    
-        
 
-
-class Responsavel(models.Model):
-    nome_completo_responsavel = models.CharField(max_length=200, verbose_name="Digite o nome do responsável completo")
-    numero_telefone_responsavel = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
-    email_responsavel = models.EmailField(max_length=100, verbose_name="Digite o e-mail do responsável")
-    adress = models.CharField(max_length=100) 
-    CPF_responsavel = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
-    data_nascimento_responsavel = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do responsável")
-    
     def __str__(self):
-        return self.nome_responsavel
-    
+        return self.turma_nome + " " + self.itinerario_nome
+
+class Materia(models.Model):
+    MATERIA_CHOICE = (
+        ("DS", "Desenvolvimento de Sistemas"),
+        ("CN", "Ciências da Natureza"),
+        ("JG", "Jogos"),
+        ("MAT", "Matemática"),
+        ("PORT", "Português"),
+        ("EF", "Educação Física"),
+        ("HIST", "História"),
+        ("GEO", "Geografia"),
+        ("ART", "Artes"),
+        ("FILO", "Filosofia"),
+        ("SOCI", "Sociologia"),
+        ("ING", "Inglês"),
+        ("ESP", "Espanhol"),
+    )
+
+    materia_nome = models.CharField(max_length=50, choices=MATERIA_CHOICE, verbose_name="Selecione o itinerário")
+
+    def __str__(self):
+        return self.materia_nome
+     
 class Aluno(models.Model):
 
-    nome_completo_aluno = models.CharField(max_length=50, verbose_name="Digite o nome do aluno completo")
-    numero_telefone_aluno = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
+    nome_completo_aluno = models.CharField(max_length=200, verbose_name="Digite o nome do aluno completo", null=True)
+    numero_telefone_aluno = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_aluno = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
-    adress = models.CharField(max_length=100) 
+    cep = models.CharField(max_length=100) 
     CPF_aluno = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
     data_nascimento_aluno = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do aluno")
     numero_matricula_aluno = models.CharField(max_length=6, verbose_name="Digite o número da sua matricula")
-    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_aluno", blank=True, null=False) 
+    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_aluno", blank=True, null=False)    
     
     def __str__(self):
         return self.nome_aluno
     
+class Responsavel(models.Model):
+    nome_completo_responsavel = models.CharField(max_length=200, verbose_name="Digite o nome do responsável completo", null=True)
+    numero_telefone_responsavel = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
+    email_responsavel = models.EmailField(max_length=100, verbose_name="Digite o e-mail do responsável")
+    cep = models.CharField(max_length=100) 
+    CPF_responsavel = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
+    data_nascimento_responsavel = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do responsável")
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name="aluno_responsavel", blank=True, null=False)
+    
+    def __str__(self):
+        return self.nome_responsavel
 
 class Professor(models.Model):
 
-    nome_completo_professor = models.CharField(max_length=50, verbose_name="Digite seu nome completo")
-    numero_telefone_professor = models.CharField(max_length=15, verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
+    nome_completo_professor = models.CharField(max_length=200, verbose_name="Digite seu nome completo", null=True)
+    numero_telefone_professor = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_professor = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
-    adress = models.CharField(max_length=100) 
+    cep = models.CharField(max_length=100) 
     CPF_professor = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
     data_nascimento_professor = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do professor")
     numero_matricula_professor = models.CharField(max_length=6, verbose_name="Digite o número da matricula")
-    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_professor", blank=True, null=False)    
-    class_choices = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="materia", blank=True, null=False) 
+    subject_choices = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="materia", blank=False, null=True) 
+    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="padrinho", blank=True, null=False)    
 
 
 #O ForeignKey é uma chave estrangeira que estabelece um relacionamento entre duas tabelas, permitindo que você vincule registros de uma tabela a registros de outra tabela. 
@@ -94,3 +90,13 @@ class Professor(models.Model):
         return self.nome_professor
     
   
+
+
+
+
+
+
+
+
+
+
