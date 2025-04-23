ff2a79b37c8640e0cd0334d54b148a6cba1ab292
Giovana <giovanadutramaga@gmail.com>
2025-04-23 11:58:36 -0300

    23-04 Initial commit

--- app-escola/escola/admin.py
+++ app-escola/escola/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from escola.models import Responsavel, Aluno, Professor, Turma, Materia
-
-class ResponsaveisAdmin(admin.ModelAdmin):
-    list_display =('nome_completo_responsavel', 'numero_telefone_responsavel', 'email_responsavel', 'CPF_responsavel', 'data_nascimento_responsavel' )
-    list_display_links = ('nome_completo_responsavel', 'numero_telefone_responsavel', 'email_responsavel')
-    search_fields = ('nome_completo_responsavel',)
-    list_filter = ('nome_completo_responsavel',)
+from escola.models import Responsavel, Aluno, Professor, Turma, Materia, Contrato
 
 class TurmaAdmin(admin.ModelAdmin):
     list_display = ('id', 'turma_nome', 'itinerario_nome')
@@ -18,23 +12,40 @@ class MateriaAdmin(admin.ModelAdmin):
     list_filter = ('materia_nome',)
    
 class AlunoAdmin(admin.ModelAdmin):
-    list_display =('nome_completo_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno', 'CPF_aluno', 'data_nascimento_aluno', 'class_choices' )
-    list_display_links = ('nome_completo_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno')
-    search_fields = ('nome_completo_aluno',)
-    list_filter = ('nome_completo_aluno',)
-    
+    list_display =('nome_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno', 'CPF_aluno', 'data_nascimento_aluno', 'class_choices', 'cep_aluno' )
+    list_display_links = ('nome_aluno', 'numero_telefone_aluno', 'email_aluno', 'numero_matricula_aluno')
+    search_fields = ('nome_aluno',)
+    list_filter = ('nome_aluno',)
+
+class ResponsaveisAdmin(admin.ModelAdmin):
+    list_display =('nome_responsavel', 'numero_telefone_responsavel', 'email_responsavel', 'CPF_responsavel', 'data_nascimento_responsavel', 'cep_responsavel' )
+    list_display_links = ('nome_responsavel', 'numero_telefone_responsavel', 'email_responsavel')
+    search_fields = ('nome_responsavel',)
+    list_filter = ('nome_responsavel',)
+
 class ProfessorAdmin(admin.ModelAdmin):
-    list_display =('nome_completo_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor', 'CPF_professor', 'data_nascimento_professor', 'subject_choices' )
-    list_display_links = ('nome_completo_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor')
-    search_fields = ('nome_completo_professor',)
-    list_filter = ('nome_completo_professor',)
-    
-    
-    
-    
-    
+    list_display =('nome_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor', 'CPF_professor', 'data_nascimento_professor', 'subject_choices', 'cep_professor' )
+    list_display_links = ('nome_professor', 'numero_telefone_professor', 'email_professor', 'numero_matricula_professor')
+    search_fields = ('nome_professor',)
+    list_filter = ('nome_professor',)
+
+class ContratoAdmin(admin.ModelAdmin):
+    list_display =('aluno', 'responsavel', 'download_contrato')
+    actions = ['download_contrato']
+    list_display_links = ('aluno', 'responsavel', 'download_contrato')
+    search_fields = ('aluno', 'responsavel',)
+    list_filter = ('aluno', 'responsavel',)
+
+#gerar um link para o downlaod do contrato   
+    def download_contrato(self, request, queryset):
+        for contrato in queryset:
+            # Aqui você pode implementar a lógica para gerar o PDF do contrato
+            # e retornar um link para download.
+            pass
+    download_contrato.short_description = "Download do Contrato"
     #Registrar as classes no Django Admin
 
+admin.site.register(Contrato, ContratoAdmin)
 admin.site.register(Turma, TurmaAdmin)
 admin.site.register(Responsavel,  ResponsaveisAdmin)
 admin.site.register(Aluno,  AlunoAdmin)
--- app-escola/escola/models.py
+++ app-escola/escola/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from escola.validate import cpf_validate, telefone_validate
+from escola.validate import cpf_validate, telefone_validate, cep_validate
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from django.shortcuts import render
 
 class Turma(models.Model):
     TURMA_CHOICE = (
@@ -45,41 +48,43 @@ class Materia(models.Model):
      
 class Aluno(models.Model):
 
-    nome_completo_aluno = models.CharField(max_length=200, verbose_name="Digite o nome do aluno completo", null=True)
+    nome_aluno = models.CharField(max_length=200, verbose_name="Digite o nome do aluno completo", null=True)
     numero_telefone_aluno = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_aluno = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
-    cep = models.CharField(max_length=100) 
-    CPF_aluno = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
+    cep_aluno = models.CharField(max_length=100, validators=[cep_validate], verbose_name="Informe o Cep do responsavel", blank=False, null=True) 
+    CPF_aluno = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=False, null=True)
     data_nascimento_aluno = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do aluno")
     numero_matricula_aluno = models.CharField(max_length=6, verbose_name="Digite o número da sua matricula")
-    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_aluno", blank=True, null=False)    
+    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="turma_aluno", blank=False, null=True)    
     
     def __str__(self):
         return self.nome_aluno
     
 class Responsavel(models.Model):
-    nome_completo_responsavel = models.CharField(max_length=200, verbose_name="Digite o nome do responsável completo", null=True)
+
+    nome_responsavel = models.CharField(max_length=200, verbose_name="Digite o nome do responsável completo", null=True)
     numero_telefone_responsavel = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_responsavel = models.EmailField(max_length=100, verbose_name="Digite o e-mail do responsável")
-    cep = models.CharField(max_length=100) 
-    CPF_responsavel = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
+    cep_responsavel = models.CharField(max_length=100, validators=[cep_validate], verbose_name="Informe o Cep do responsavel", blank=False, null=True) 
+    CPF_responsavel = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=False, null=True)
     data_nascimento_responsavel = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do responsável")
-    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name="aluno_responsavel", blank=True, null=False)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name="aluno_responsavel", blank=False, null=True)
     
     def __str__(self):
         return self.nome_responsavel
+    
 
 class Professor(models.Model):
 
-    nome_completo_professor = models.CharField(max_length=200, verbose_name="Digite seu nome completo", null=True)
+    nome_professor = models.CharField(max_length=200, verbose_name="Digite seu nome completo", null=True)
     numero_telefone_professor = models.CharField(max_length=15, validators=[telefone_validate], verbose_name="Digite o número do celular (xx) xxxxx-xxxx")
     email_professor = models.EmailField(max_length=100, verbose_name="Digite o seu e-mail")
-    cep = models.CharField(max_length=100) 
-    CPF_professor = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=True, null=False)
+    cep_professor = models.CharField(max_length=100, validators=[cep_validate], verbose_name="Informe o Cep do responsavel", blank=False, null=True) 
+    CPF_professor = models.CharField(max_length=11, validators=[cpf_validate], verbose_name="Informe o CPF do responsável", blank=False, null=True)
     data_nascimento_professor = models.DateField(max_length=10, verbose_name="Digite a data de nascimento do professor")
     numero_matricula_professor = models.CharField(max_length=6, verbose_name="Digite o número da matricula")
     subject_choices = models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="materia", blank=False, null=True) 
-    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="padrinho", blank=True, null=False)    
+    class_choices = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="padrinho", blank=False, null=True)    
 
 
 #O ForeignKey é uma chave estrangeira que estabelece um relacionamento entre duas tabelas, permitindo que você vincule registros de uma tabela a registros de outra tabela. 
@@ -88,7 +93,48 @@ class Professor(models.Model):
 
     def __str__(self):
         return self.nome_professor
+
+class Contrato(models.Model): 
+
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, related_name="contrato_aluno", blank=False, null=True)
+    responsavel = models.ForeignKey(Responsavel, on_delete=models.CASCADE, related_name="contrato_responsavel", blank=False, null=True)
+
+    def mm2p(milimetros):
+        return milimetros / 0.352777
+
+    def contrato(self):
+        eixo = 100
+        cnv = canvas.Canvas("contrato_pdf.pdf", pagesize= A4)
+        cnv.drawString(self.mm2p(eixo),self.mm2p(eixo), self.aluno.nome_aluno, 
+                       self.aluno.numero_telefone_aluno,
+                       self.aluno.email_aluno,
+                       self.aluno.cep_aluno,
+                       self.aluno.CPF_aluno,
+                       self.aluno.data_nascimento_aluno,
+                       self.aluno.numero_matricula_aluno,
+                       self.aluno.class_choices,
+                       self.responsavel.nome_responsavel,
+                       self.responsavel.numero_telefone_responsavel,
+                       self.responsavel.email_responsavel,
+                       self.responsavel.cep_responsavel,
+                       self.responsavel.CPF_responsavel,
+                       self.responsavel.data_nascimento_responsavel)
+        eixo -= 20
+        cnv.save()    
+
+def gerar_contrato(request, aluno_id, responsavel_id):
+    aluno = Aluno.objects.get(id=aluno_id)
+    responsavel = Responsavel.objects.get(id=responsavel_id)
+    contrato = Contrato.objects.create(aluno=aluno, responsavel=responsavel)
     
+    contrato.contrato()  # Gera o contrato PDF
+    
+    # Pode retornar um link para o PDF gerado ou exibir uma confirmação
+    return render(request, 'contrato.html', {'contrato_pdf': 'contrato_pdf.pdf'})
+
+
+
+
   
 
 
